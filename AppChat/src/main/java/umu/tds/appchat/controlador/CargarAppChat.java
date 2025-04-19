7182004b109264dabbecd5be9fff89febcaf4745
fjramirez1 <fj.ramirezlopez@um.es>
2025-04-19 11:09:42 +0200

    CargarAppChat para tutoria (¿cambiar fotos?)

--- AppChat/src/main/java/umu/tds/appchat/controlador/CargarAppChat.java
+++ AppChat/src/main/java/umu/tds/appchat/controlador/CargarAppChat.java
@@ -4,6 +4,7 @@ import java.awt.EventQueue;
 import java.time.LocalDate;
 
 import umu.tds.appchat.dominio.ContactoIndividual;
+import umu.tds.appchat.dominio.RepositorioUsuarios;
 import umu.tds.appchat.dominio.TipoDescuento;
 import umu.tds.appchat.vista.VentanaLogin;
 
@@ -25,26 +26,33 @@ public class CargarAppChat {
 	 * @param args argumentos de línea de comandos (no se utilizan)
 	 */
 	public static void main(String[] args) {
-		AppChat.getInstance().registrarUsuario("javi", "1", "1", LocalDate.now(), "src/main/resources/profile1.jpg", "hola", "javi@a.com");
+		AppChat.getInstance().registrarUsuario("aa", "1", "1", LocalDate.now(), "src/main/resources/profile1.jpg", "hola", "javi@a.com");
+		AppChat.getInstance().registrarUsuario("bb", "2", "2", LocalDate.now(), "src/main/resources/profile1.jpg", "hola", "angel@a.com");
+		AppChat.getInstance().registrarUsuario("cc", "3", "3", LocalDate.now(), "src/main/resources/profile1.jpg", "hola", "pepe@a.com");
+		AppChat.getInstance().registrarUsuario("dd", "4", "4", LocalDate.now(), "src/main/resources/profile1.jpg", "hola", "diego@a.com");
+		AppChat.getInstance().registrarUsuario("ee", "5", "5", LocalDate.now(), "src/main/resources/profile1.jpg", "hola", "elena@a.com");
+		
 		AppChat.getInstance().login("1", "1");
-		AppChat.getInstance().activarPremium(TipoDescuento.FECHA);
-		AppChat.getInstance().anularPremium();
 		
-		AppChat.getInstance().registrarUsuario("angel", "2", "2", LocalDate.now(), "src/main/resources/profile1.jpg", "hola", "angel@a.com");
-		AppChat.getInstance().login("2", "2");
+		ContactoIndividual c1 = AppChat.getInstance().agregarContactoIndividual("angel", "2");
+		ContactoIndividual c2 = AppChat.getInstance().agregarContactoIndividual("pepe", "3");
 		
-		AppChat.getInstance().registrarUsuario("jesus", "3", "3", LocalDate.now(), "src/main/resources/profile1.jpg", "hola", "jesus@a.com");
-		AppChat.getInstance().login("3", "3");
-		
-		ContactoIndividual c1 = AppChat.getInstance().agregarContactoIndividual("javi", "1");
-		ContactoIndividual c2 = AppChat.getInstance().agregarContactoIndividual("angel", "2");
-		AppChat.getInstance().enviarMensajeContacto(c1, "hola", -1);
-		AppChat.getInstance().enviarMensajeContacto(c1, "que tal", -1);
+		AppChat.getInstance().enviarMensajeContacto(c1, "Hola, ¿cómo estás?", -1);
 		AppChat.getInstance().enviarMensajeContacto(c1, "", 2);
 		
-		AppChat.getInstance().enviarMensajeContacto(c2, "hola", -1);
-		AppChat.getInstance().enviarMensajeContacto(c2, "que tal", -1);
-		AppChat.getInstance().enviarMensajeContacto(c2, "", 2);
+		AppChat.getInstance().enviarMensajeContacto(c2, "Cuando cantas?", -1);
+		AppChat.getInstance().enviarMensajeContacto(c2, "", 6);
+		
+		AppChat.getInstance().login("2", "2");
+		
+		//ContactoIndividual c3 = AppChat.getInstance().agregarContactoIndividual("javi", "1");
+		ContactoIndividual c3 = RepositorioUsuarios.INSTANCE.buscarUsuarioPorMovil("2").getContactoIndividual("1");
+		ContactoIndividual c4 = AppChat.getInstance().agregarContactoIndividual("diego", "4");
+		ContactoIndividual c5 = AppChat.getInstance().agregarContactoIndividual("elena", "5");
+		
+		AppChat.getInstance().enviarMensajeContacto(c3, "Vienes este finde?", -1);
+		AppChat.getInstance().enviarMensajeContacto(c3, "", 3);
+		AppChat.getInstance().enviarMensajeContacto(c4, "Juegas esta semana?", -1);
 		
 	    System.out.println("Fin de la carga de datos");
 	    
